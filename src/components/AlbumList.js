ca1f0dc9da9f9d1abf63d1a29f30393a9bd19078
jp853 <jpingatore@gmail.com>
2018-04-19 07:53:47 -0600

    added in component will mount lifecycle with axios get request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,14 @@
 /*jshint esversion: 6 */
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  componetWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
+  }
+
   render() {
     return (
       <View>
